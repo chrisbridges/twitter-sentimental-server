cb4a60bb59bc163f569b93ea11b5395e35fcfdf0
chris.bridges <christopher_bridges@icloud.com>
2018-05-30 15:40:20 -0400

    basic stock stream GET endpoint implemented

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const Twitter = require('twitter');
 const Sentiment = require('sentiment');
-const {CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET} = require('./config');
+const {PORT, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET} = require('./config');
 
 app.use(morgan('common'));
-app.use(express.static('public'));
-app.use(bodyParser.json());
+// app.use(express.static('public'));
+// app.use(bodyParser.json());
 
 const twitter = new Twitter({
   consumer_key: CONSUMER_KEY,
@@ -41,13 +41,63 @@ const twitter = new Twitter({
 
 const sentiment = new Sentiment();
 
-const stream = twitter.stream('statuses/filter', {track: '$AAPL'});
-  stream.on('data', function(event) {
-    console.log(event && event.text);
-    const sentimentScore = sentiment.analyze(event.text).score;
+app.get('/stocks/:stock', function (req, res) {
+  const {stock} = req.params;
+  const stream = twitter.stream('statuses/filter', {track: `$${stock}`});
+  stream.on('data', function(tweet) {
+    console.log(tweet && tweet.text);
+    const sentimentScore = sentiment.analyze(tweet.text).score;
     console.log(sentimentScore);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
 });
 
-stream.on('error', function(error) {
-  throw error;
-});
+let server;
+
+function runServer(port = PORT) {
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve();
+    })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+// app.use('*', function (req, res) {
+//   res.status(404).sendFile(__dirname + '/public/page-not-found.html');
+// });
+
+// const stream = twitter.stream('statuses/filter', {track: '$AAPL'});
+//   stream.on('data', function(event) {
+//     console.log(event && event.text);
+//     const sentimentScore = sentiment.analyze(event.text);
+//     console.log(sentimentScore);
+// });
+
+// stream.on('error', function(error) {
+//   throw error;
+// });
+
+module.exports = {app, runServer, closeServer};
