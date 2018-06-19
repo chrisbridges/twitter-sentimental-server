917666b1e28975c7f80b95b19c1206398e51a4a2
chris.bridges <christopher_bridges@icloud.com>
2018-06-19 18:38:59 -0400

    fixed client origin typo

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 exports.PORT = process.env.PORT || 8080;
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http:localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http:localhost:3000';
 exports.CONSUMER_KEY = process.env.CONSUMER_KEY;
 exports.CONSUMER_SECRET = process.env.CONSUMER_SECRET;
 exports.ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY;
