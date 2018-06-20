53bfec3b73d0932196d791f17eea51b5f2a7efdd
chris.bridges <christopher_bridges@icloud.com>
2018-06-20 13:05:51 -0400

    older version of socket

--- src/store.js
+++
@@ -1,5 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import {stockReducer} from './reducers/stockSearchReducer';
-
-export default createStore(stockReducer, applyMiddleware(thunk));
